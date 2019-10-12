a9a4c518f69a499b4b331c5288a15544a6f9c87f
Lucas Vieira <tiomedianope@gmail.com>
2019-10-11 22:01:59 -0300

    Desafio Intermediário - Aula 014

--- js/description-product.js
+++ js/description-product.js
@@ -1,6 +1,21 @@
 
-const $heart = window.document.querySelector(".-heart")
+const $heart = document.querySelector(".-heart")
+const $fatherStar = document.querySelector('.-star')
+
+$fatherStar.addEventListener("click", function(e) {
+    const elem = e.target
+    e.target.addEventListener("click", handleStar(elem))
+    if ($fatherStar.classList.contains('-active')) {
+        $fatherStar.classList.remove('-active')
+    }
+});
+
 $heart.addEventListener("click", handleClick)
+
+function handleStar(e) {
+    console.log(e)
+    e.classList.toggle('-active')   
+}
 
 function handleClick() {
     // if ($heart.classList.contains('-active')) {
