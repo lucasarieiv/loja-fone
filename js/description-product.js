6e31e8209ff2a8124486e8e6dba3bb6e4d3f7894
Lucas Vieira <tiomedianope@gmail.com>
2019-10-14 16:58:11 -0300

    Aula 015

--- js/description-product.js
+++ js/description-product.js
@@ -1,28 +1,15 @@
 
 const $heart = document.querySelector(".-heart")
-const $fatherStar = document.querySelector('.-star')
-
-$fatherStar.addEventListener("click", function(e) {
-    const elem = e.target
-    e.target.addEventListener("click", handleStar(elem))
-    if ($fatherStar.classList.contains('-active')) {
-        $fatherStar.classList.remove('-active')
-    }
-});
+const $stars = document.querySelectorAll('.star')
+let index = 0
 
 $heart.addEventListener("click", handleClick)
 
-function handleStar(e) {
-    console.log(e)
-    e.classList.toggle('-active')   
-}
+$stars.forEach(function($star, key) {
+    $star.addEventListener("click", handleClick)
+})
+
 
 function handleClick() {
-    // if ($heart.classList.contains('-active')) {
-    //     $heart.classList.remove('-active')
-    // } else {
-    //     $heart.classList.add('-active')
-    // }
-    $heart.classList.toggle("-active")
+    this.classList.toggle('-active')
 }
-
