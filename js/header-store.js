628bbffcb271f4c5b7e7d178347b7b1b0c7e85d4
Lucas Vieira <tiomedianope@gmail.com>
2019-10-16 18:05:36 -0300

    Desafio Avançado - Aula 017

--- js/header-store.js
+++ js/header-store.js
@@ -4,5 +4,6 @@ const $input = document.querySelector("[type='search']")
 
 $search.addEventListener("click", () => {
     $input.classList.toggle('-active')
+    $search.classList.toggle('-active')
 })
 
