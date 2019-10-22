04992c7e82b470ab7c3b962ad91d5fae976fc601
lucasarieiv <tiomedianope@gmail.com>
2019-10-22 10:44:14 -0300

    Aula - 018

--- js/header-store.js
+++
@@ -1,9 +0,0 @@
-const $search = document.querySelector('.search-icon')
-const $input = document.querySelector("[type='search']")
-
-
-$search.addEventListener("click", () => {
-    $input.classList.toggle('-active')
-    $search.classList.toggle('-active')
-})
-
